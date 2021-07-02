6287f863529eea887431a16e356dcbe350f0d64e
Mariem-Hamraoui <mariamhamraoui@gmail.com>
2021-07-02 02:00:23 +0100

    updated strucutre stylin + fct

--- bmicalculator/script.js
+++ bmicalculator/script.js
@@ -25,13 +25,16 @@ $("#bmi").click(function () {
   var BMI;
   var height = ($('#height').val())*0.1;
   var weight = $('#weight').val() ;
-  BMI = Math.trunc((weight / (height * height) ) * 100) ;
-  alert(" Your Body Mass Index is  " + BMI + "  kg/m2 ")
+  BMI = Math.trunc((weight / (height * height) ) * 100) ; 
+  
+  if( 18.5 < BMI &&  BMI < 25)
+  alert("  Your Body Mass Index is  " + BMI + "  kg/m2  is Normal  .. Good!! ") ;
+   
+  else ( BMI < 18.25 &&  25 < BMI ) 
+  alert (" Your Body Mass Index is  " + BMI + "  kg/m2   .. take heed! " );
   $('#height').empty();
   $('#Weight').empty();
 })
-
-
 
 
 
